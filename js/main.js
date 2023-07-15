46a183d8bf876fbfaebf398d5a4cfaa0dfe7199a
sunpaaaa <sunpaaaa@gmail.com>
2023-07-15 13:53:52 +0900

    Add main top banner

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
 });
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.6,
+    opacity: 1,
+  });
+});
